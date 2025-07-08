049cbfb459a4b8159992637cdcb5f68af8e5948f
BuiHoangThanhLam <builam009@gmail.com>
2025-07-08 15:36:58 +0700

    bttb2

--- MVC/Models/Person.cs
+++ MVC/Models/Person.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace MvcMovie.Models;
 
+[Table("Person")]
 public class Person
 {
+    [Key]
     public string? PersonId { get; set; }
     public string? FullName { get; set; }
     public string? Address { get; set; }
